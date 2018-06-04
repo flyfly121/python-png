f94f52a69a25914306af0d2647528f700188e181
sunxianglin <sunxianglin@qiyi.com>
2018-06-04 17:31:03 +0800

    Byte Order >

--- utils.py
+++ utils.py
@@ -6,8 +6,9 @@ import struct
 def parse_length(filename, size):
     chunk_length = filename.read(size)
     if size > 1:
-        chunk_length = chunk_length[::-1]
-        chunk_length, = struct.unpack('I', chunk_length)
+        # chunk_length = chunk_length[::-1]
+        # https://docs.python.org/3/library/struct.html#byte-order-size-and-alignment
+        chunk_length, = struct.unpack('>I', chunk_length)
     elif size == 1:
         chunk_length, = struct.unpack('b', chunk_length)
     return chunk_length
